05a62ee0412405070bf9f083a68db25de1b1e2ad
davelevine <dave@davelevine.io>
2025-03-23 16:27:18 -0400

    Changed chunk size to stay under Vercel API limit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@ export const MB = 10 ** 6 // 1000000 Bytes = 1 MB.
 export const GB = 10 ** 9 // 1000000000 Bytes = 1 GB.
 
 export const getMaxFileSize = (env: string) => (env === 'production' ? 10 : 1) * GB
-export const getChunkSize = (env: string) => (env === 'production' ? 100 : 1) * MB
+export const getChunkSize = (env: string) => (env === 'production' ? 3 : 1) * MB
