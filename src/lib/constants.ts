1ad323d9d754818bdd9e99cf544d4123364f6b53
davelevine <dave@davelevine.io>
2025-03-26 17:18:46 -0400

    Updated chunk size

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@ export const MB = 10 ** 6 // 1000000 Bytes = 1 MB.
 export const GB = 10 ** 9 // 1000000000 Bytes = 1 GB.
 
 export const getMaxFileSize = (env: string) => (env === 'production' ? 10 : 1) * GB
-export const getChunkSize = (env: string) => (env === 'production' ? 3.2 : 1) * MB
+export const getChunkSize = (env: string) => (env === 'production' ? 8 : 1) * MB
